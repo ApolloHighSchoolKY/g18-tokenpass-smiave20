5a51c38d390a73261af68707e697172d70179d99
smiave20 <avery.smith@stu.daviess.kyschools.us>
2026-02-04 17:54:41 +0000

    It's bad

--- TokenPass.java
+++ TokenPass.java
@@ -1,6 +1,15 @@
 public TokenPass(int playerCount){
   /*  to be answered in part (a) */
   
+  //Creates board Array to be the size of playerCount
+  board = new int[playerCount];
+
+  //Fills it with random integer values from 1 to 10, 10 inclusive.
+  playerCount = (Math.random()*10) +1;
+
+  //Initializes currentPlayer to a random integer value in the range between 0 and playerCount-1, inclusive.
+  for(int currentPlayer= 0; currentPlayer > playerCount-1; currentPlayer ++)
+    
 }
 
 public void distributeCurrentPlayerTokens(){
